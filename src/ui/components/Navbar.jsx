a3f64ebb2f6905e940535adae679ece35cf34975
Francisco Roman <franciscoromansierra@gmail.com>
2024-05-30 23:58:50 -0500

    Agregue navegacion activa

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -46,7 +46,7 @@ export function Navbar() {
             <li>
               <NavLink
                 to="/home"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className={({isActive}) => `block py-2 px-3 text-white rounded md:p-0 ${isActive ? 'bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500' : ''}`}
                 aria-current="page"
               >
                 Home
@@ -55,7 +55,7 @@ export function Navbar() {
             <li>
               <NavLink
                 to="/store"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={({isActive}) => `block py-2 px-3 text-white rounded md:p-0 ${isActive ? 'bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500' : ''}`}
               >
                 Store
               </NavLink>
@@ -63,7 +63,7 @@ export function Navbar() {
             <li>
               <NavLink
                 to="/cart"
-                className="flex items-center gap-1 py-2 px-3 text-gray-900 rounded md:border-0 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={({isActive}) => `flex items-center gap-1 py-2 px-3 text-white rounded md:p-0 ${isActive ? 'bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500' : ''}`}
               >
                 Cart
                 <svg
