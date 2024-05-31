3b8558e4170c800afec91cfb735791b4485c160b
Francisco Roman <franciscoromansierra@gmail.com>
2024-05-30 19:49:46 -0500

    inicial react route

--- src/ECommersApp.jsx
+++ src/ECommersApp.jsx
@@ -1,3 +1,10 @@
 export function ECommersApp () {
     return <h1 className="text-3xl">Change title app</h1>
 }
+
+/** 
+ * Tarea Crear 3 componentes 
+ * 1 - HomePage.jsx
+ * 2 - StorePage.jsx
+ * 3 - CartPage.jsx
+ */
---
+++ src/e-commers/pages/StorePage.jsx
@@ -0,0 +1,3 @@
+export function StorePage () {
+    return <h1 className="text-2xl font-bold">Store Page</h1>
+}
