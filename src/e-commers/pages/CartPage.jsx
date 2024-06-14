006c6bcdbe3ba2572e191a72f5f75644e828509e
Francisco Roman <franciscoromansierra@gmail.com>
2024-06-13 20:50:10 -0500

    Agregue validacion en la vista del carrito de compras

--- src/e-commers/pages/CartPage.jsx
+++ src/e-commers/pages/CartPage.jsx
@@ -2,11 +2,18 @@ import { useCart } from "../../context/ContextCart"
 
 export function CartPage () {
     const { cart } = useCart();
-    console.log(cart);
+    
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-4xl font-bold">Shopping Cart</h1>
-            <p className="text-xl mt-4">Your cart is empty.</p>
+            {
+                cart.length === 0 ?
+                    (
+                        <p className="text-xl mt-4">Your cart is empty.</p>
+                    ) : (
+                        <p>{JSON.stringify(cart)}</p>
+                    )
+            }
         </div>
     )
 }
